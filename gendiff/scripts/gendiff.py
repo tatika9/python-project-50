57b1457f7f0feee8dbc3831a79a4aa16bcbf6433
Rinat Shakirov <tatika0872@gmail.com>
2023-07-05 19:33:00 +0500

    feat: add flag -f

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,6 +10,10 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output'
+    )
     args = parser.parse_args()
 
 if __name__ == '__main__':
