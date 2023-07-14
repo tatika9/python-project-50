fe45d4189ceb4126ac87df7e4ea6fce8b2451e04
Rinat Shakirov <tatika0872@gmail.com>
2023-07-14 15:55:18 +0500

    refactor: string length

--- gendiff/format.py
+++ gendiff/format.py
@@ -1,5 +1,6 @@
 import itertools
-from gendiff.diff_tree import is_nested, get_action, get_name, get_value, get_children
+from gendiff.diff_tree import is_nested, get_action, get_name
+from gendiff.diff_tree import get_value, get_children
 
 
 def get_format(diff, format):
@@ -28,7 +29,8 @@ def stylish(diff):
             lines = []
             current_indent = (level * indent - len(prefix_unchange)) * ' '
             for key, val in diff.items():
-                lines.append(f'{current_indent}{prefix_unchange}{key}:{format_unchange(val, level + 1)}')
+                lines.append(f'{current_indent}{prefix_unchange}{key}:'
+                             f'{format_unchange(val, level + 1)}')
             current_indent = (level - 1) * indent * ' '
             result = itertools.chain([' {'], lines, [current_indent + '}'])
             return '\n'.join(result)
@@ -41,16 +43,34 @@ def stylish(diff):
 
         for child in children:
             if is_nested(child):
-                lines.append(f'{current_indent}{prefix_unchange}{get_name(child)}: {inner(child, level + 1)}')
+                lines.append(
+                    f'{current_indent}{prefix_unchange}'
+                    f'{get_name(child)}: {inner(child, level + 1)}'
+                )
             elif get_action(child) == 'deleted':
-                lines.append(f'{current_indent}{prefix_deleted}{get_name(child)}:{format_unchange(get_value(child)["old"], level+1)}')
+                lines.append(
+                    f'{current_indent}{prefix_deleted}{get_name(child)}:'
+                    f'{format_unchange(get_value(child)["old"], level+1)}'
+                )
             elif get_action(child) == 'added':
-                lines.append(f'{current_indent}{prefix_added}{get_name(child)}:{format_unchange(get_value(child)["new"], level+1)}')
+                lines.append(
+                    f'{current_indent}{prefix_added}{get_name(child)}:'
+                    f'{format_unchange(get_value(child)["new"], level+1)}'
+                )
             elif get_action(child) == 'changed':
-                lines.append(f'{current_indent}{prefix_deleted}{get_name(child)}:{format_unchange(get_value(child)["old"], level+1)}')
-                lines.append(f'{current_indent}{prefix_added}{get_name(child)}:{format_unchange(get_value(child)["new"], level+1)}')
+                lines.append(
+                    f'{current_indent}{prefix_deleted}{get_name(child)}:'
+                    f'{format_unchange(get_value(child)["old"], level+1)}'
+                )
+                lines.append(
+                    f'{current_indent}{prefix_added}{get_name(child)}:'
+                    f'{format_unchange(get_value(child)["new"], level+1)}'
+                )
             elif get_action(child) == 'unchanged':
-                lines.append(f'{current_indent}{prefix_unchange}{get_name(child)}:{format_unchange(get_value(child)["old"], level+1)}')
+                lines.append(
+                    f'{current_indent}{prefix_unchange}{get_name(child)}:'
+                    f'{format_unchange(get_value(child)["old"], level+1)}'
+                )
         current_indent = (level - 1) * indent * ' '
         result = itertools.chain('{', lines, [current_indent + '}'])
         return '\n'.join(result)
--- gendiff/parser_diff.py
+++ gendiff/parser_diff.py
@@ -8,14 +8,22 @@ def get_diff(dict1, dict2):
         keys = sorted(node1.keys() | node2.keys())
         for key in keys:
             if key not in node1:
-                children.append(mk_plain(key, 'added', {'old': None, 'new': node2[key]}))
+                children.append(mk_plain(
+                    key, 'added', {'old': None, 'new': node2[key]}
+                ))
             elif key not in node2:
-                children.append(mk_plain(key, 'deleted', {'old': node1[key], 'new': None}))
+                children.append(mk_plain(
+                    key, 'deleted', {'old': node1[key], 'new': None}
+                ))
             elif isinstance(node1[key], dict) and isinstance(node2[key], dict):
                 children.append(iter(key, node1[key], node2[key]))
             elif node1[key] == node2[key]:
-                children.append(mk_plain(key, 'unchanged', {'old': node1[key], 'new': node2[key]}))
+                children.append(mk_plain(
+                    key, 'unchanged', {'old': node1[key], 'new': node2[key]}
+                ))
             else:
-                children.append(mk_plain(key, 'changed', {'old': node1[key], 'new': node2[key]}))
+                children.append(mk_plain(
+                    key, 'changed', {'old': node1[key], 'new': node2[key]}
+                ))
         return mk_nested(name, children)
     return iter(None, dict1, dict2)
